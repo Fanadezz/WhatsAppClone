4f74516cab72f6ef3bc9026a8c55ea17b8054bae
Antony <vontonnie@gmail.com>
2020-09-29 18:29:01 +0300

    Check if the user is already signed in

--- app/src/main/java/com/androidshowtime/whatsappclone/LoginFragment.kt
+++ app/src/main/java/com/androidshowtime/whatsappclone/LoginFragment.kt
@@ -1,16 +1,17 @@
 package com.androidshowtime.whatsappclone
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.androidshowtime.whatsappclone.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class LoginFragment : Fragment() {
 
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,6 +20,18 @@ class LoginFragment : Fragment() {
 
         //create binding
         val binding = FragmentLoginBinding.inflate(inflater)
+
+
+        //check whether a user is already signed in from a previous session
+
+        if (auth.currentUser != null) {
+
+
+            //already signed in
+        } else {
+
+            //not signed in
+        }
 
 
         return binding.root
