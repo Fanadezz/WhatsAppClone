1708d0908630a5d4ff0afddc75b4c9985ab261c7
Antony <vontonnie@gmail.com>
2020-09-30 19:45:28 +0300

    Implement Log out

--- app/src/main/java/com/androidshowtime/whatsappclone/ChatFragment.kt
+++ app/src/main/java/com/androidshowtime/whatsappclone/ChatFragment.kt
@@ -1,26 +1,48 @@
 package com.androidshowtime.whatsappclone
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.androidshowtime.whatsappclone.databinding.FragmentChatBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class ChatFragment : Fragment() {
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentChatBinding.inflate(inflater)
 
+        //show menu
+        setHasOptionsMenu(true)
+
+        return binding.root
     }
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?): View? {
-        val binding =FragmentChatBinding.inflate(inflater)
-        return binding.root
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+//Log out
+            R.id.log_out -> {
+                FirebaseAuth.getInstance().signOut()
+                findNavController().navigate(ChatFragmentDirections.actionChatFragmentToLoginFragment())
+                true
+            }
+
+
+            else -> return super.onOptionsItemSelected(item)
+        }
+
+
     }
 
 
