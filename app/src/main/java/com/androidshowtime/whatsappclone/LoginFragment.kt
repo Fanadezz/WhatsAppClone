6a478b5029f6eb938bffbcea5fc226c1af526c3d
Antony <vontonnie@gmail.com>
2020-09-30 07:43:06 +0300

    Add image and login button

--- app/src/main/java/com/androidshowtime/whatsappclone/LoginFragment.kt
+++ app/src/main/java/com/androidshowtime/whatsappclone/LoginFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.androidshowtime.whatsappclone.databinding.FragmentLoginBinding
+import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 
 
@@ -13,28 +15,50 @@ class LoginFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+                             ): View? {
+        //initialize auth
+        auth = FirebaseAuth.getInstance()
 
         //create binding
         val binding = FragmentLoginBinding.inflate(inflater)
 
 
         //check whether a user is already signed in from a previous session
-
         if (auth.currentUser != null) {
-
-
             //already signed in
-        } else {
+
+        }
+        else {
 
             //not signed in
         }
 
 
         return binding.root
+    }
+
+
+    //start signin proces
+
+    fun signin() {
+
+
+        //choose Authentication providers
+        val providers = arrayListOf(
+                AuthUI.IdpConfig.PhoneBuilder().build(),
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.TwitterBuilder().build())
+
+
+        //create and launch sign-in intent
+        ActivityResultContracts.StartActivityForResult(AuthUI.getInstance()
+                                                               .createSignInIntentBuilder()
+                                                               .setAvailableProviders(providers)
+                                                               .build(), 12)
+
+
     }
 
 
