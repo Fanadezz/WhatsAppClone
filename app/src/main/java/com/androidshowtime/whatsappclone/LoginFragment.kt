52fe6e0ec1c1b9078ab880007ad0dc6ff12f3ebe
Antony <vontonnie@gmail.com>
2020-09-30 09:03:30 +0300

    Accomplish Login

--- app/src/main/java/com/androidshowtime/whatsappclone/LoginFragment.kt
+++ app/src/main/java/com/androidshowtime/whatsappclone/LoginFragment.kt
@@ -1,17 +1,42 @@
 package com.androidshowtime.whatsappclone
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.androidshowtime.whatsappclone.databinding.FragmentLoginBinding
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.AuthUI.IdpConfig
+import com.firebase.ui.auth.AuthUI.IdpConfig.PhoneBuilder
 import com.google.firebase.auth.FirebaseAuth
 
 
 class LoginFragment : Fragment() {
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+
+
+
+
+
+        if (result.resultCode == Activity.RESULT_OK) {
+           // val intent = result.intent
+            // Handle the Intent
+
+
+
+
+        }
+
+
+
+
+
+    }
 
     private lateinit var auth: FirebaseAuth
     override fun onCreateView(
@@ -33,6 +58,39 @@ class LoginFragment : Fragment() {
         else {
 
             //not signed in
+        }
+
+        //login Button implementation
+
+        binding.loginButton.setOnClickListener {
+
+            val whitelistedCountries: MutableList<String> = ArrayList()
+            whitelistedCountries.add("+254")
+
+
+            val phoneConfigWithWhitelistedCountries = PhoneBuilder()
+                    .setWhitelistedCountries(whitelistedCountries)
+                    .build()
+
+
+
+
+            //val providers = arrayListOf(AuthUI.IdpConfig.PhoneBuilder().build())
+            val providers = arrayListOf(phoneConfigWithWhitelistedCountries)
+
+
+
+            val intent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .setTosAndPrivacyPolicyUrls("https://example.com", "https://example.com")
+                    .setAlwaysShowSignInMethodScreen(true)
+                    .build()
+
+            startForResult.launch(intent)
+
+
+
         }
 
 
