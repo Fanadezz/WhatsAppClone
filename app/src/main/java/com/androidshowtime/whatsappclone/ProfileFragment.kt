f59e13c852fe406b8f6281591d92b488746ed65e
Antony <vontonnie@gmail.com>
2020-10-09 15:01:30 +0300

    Add OK Button Implementation

--- app/src/main/java/com/androidshowtime/whatsappclone/ProfileFragment.kt
+++ app/src/main/java/com/androidshowtime/whatsappclone/ProfileFragment.kt
@@ -6,13 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.androidshowtime.whatsappclone.databinding.FragmentProfileBinding
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 
 class ProfileFragment : Fragment() {
 //vals
     private val args:ProfileFragmentArgs by navArgs()
+
+    //vars
+    private lateinit var name:String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +33,27 @@ class ProfileFragment : Fragment() {
 
         //set phone number
         binding.phoneNumberTexView.text = args.phoneNumber
+
+        //on OK button click
+
+        binding.okayButton.setOnClickListener {
+            //get value of the entered name
+            if (binding.editTextTextPersonName.text.isNotBlank()){
+
+                name = binding.editTextTextPersonName.text.toString()
+
+
+            }
+
+            else{
+
+
+
+                Snackbar.make(binding.root, "Please Enter your Name", Snackbar.LENGTH_SHORT).show()
+            }
+
+            findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToChatFragment())
+        }
 
         // return root
         return binding.root
