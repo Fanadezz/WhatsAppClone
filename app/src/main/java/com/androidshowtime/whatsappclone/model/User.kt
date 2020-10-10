b91e3123832723d1e97dda5d5849e187f7766c45
Antony <vontonnie@gmail.com>
2020-10-10 18:02:15 +0300

    Parcelize User

--- app/src/main/java/com/androidshowtime/whatsappclone/model/User.kt
+++ app/src/main/java/com/androidshowtime/whatsappclone/model/User.kt
@@ -1,3 +1,7 @@
 package com.androidshowtime.whatsappclone.model
 
-data class User(val phoneNumber: String? = null, val  name:String? = null)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class User(val phoneNumber: String? = null, val  name:String? = null): Parcelable
