fd2a640ab51f90713afc30831c7f1537f9074c34
Antony <vontonnie@gmail.com>
2020-10-12 08:01:47 +0300

    Implement getItemCount method

--- app/src/main/java/com/androidshowtime/whatsappclone/RecyclerViewAdapter.kt
+++ app/src/main/java/com/androidshowtime/whatsappclone/RecyclerViewAdapter.kt
@@ -16,15 +16,19 @@ class RecyclerViewAdapter(private val messageDataSet: MutableList<Message>) :
 
     //inflate the recyclerView's row xml layout and return a ViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
+        //called a number of times depending on number of rows for initial display n the
+        //number of rows needed to manage recycling
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.recycler_view_layout, parent, false)
 
 
-        //return an instance of the inner ViewHOlderClass
+        //return an instance of the inner ViewHolderClass
         return ViewHolderClass(itemView)
     }
-
+//bind or maps data to the children
     override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
+
+    //it is called for each row
         TODO("Not yet implemented")
     }
 
