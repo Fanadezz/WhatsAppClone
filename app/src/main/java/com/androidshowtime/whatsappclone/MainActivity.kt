e0cfc9915bc58492982a6c81ea3483d73e84b55a
Antony <vontonnie@gmail.com>
2020-11-09 15:38:15 +0300

    Updated Libraries

--- app/src/main/java/com/androidshowtime/whatsappclone/MainActivity.kt
+++ app/src/main/java/com/androidshowtime/whatsappclone/MainActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_chat.*
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         Timber.plant(Timber.DebugTree())
+
+
+
     }
 }
