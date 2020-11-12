f1956ade1a7272cc957e2720e6908c039d95339e
Antony <vontonnie@gmail.com>
2020-11-12 05:39:38 +0300

    Victor's Changes

--- app/src/main/java/com/androidshowtime/whatsappclone/RecyclerViewAdapter.kt
+++ app/src/main/java/com/androidshowtime/whatsappclone/RecyclerViewAdapter.kt
@@ -3,10 +3,15 @@ package com.androidshowtime.whatsappclone
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewDebug
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.androidshowtime.whatsappclone.databinding.IncomingMsgLayoutBinding
+import com.androidshowtime.whatsappclone.databinding.OutgoingMsgLayoutBinding
 import com.androidshowtime.whatsappclone.model.Message
+import com.google.firebase.auth.FirebaseAuth
 import timber.log.Timber
 import java.text.SimpleDateFormat
 import java.util.*
@@ -15,10 +20,10 @@ import java.util.*
 /*Add RecyclerViewAdapter class responsible for displaying message items. The Adapter
 Class extends RecyclerView.Adapter and we pass in a ViewHolder Class*/
 class RecyclerViewAdapter(private val messageDataSet: MutableList<Message>) :
-        RecyclerView.Adapter<RecyclerViewAdapter.ViewHolderClass>() {
+        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
 
-    //inflate the recyclerView's row xml layout and return a ViewHolder
+  /*  //inflate the recyclerView's row xml layout and return a ViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
 
         //called a number of times depending on number of rows for initial display n the
@@ -37,7 +42,7 @@ class RecyclerViewAdapter(private val messageDataSet: MutableList<Message>) :
         return LayoutInflater.from(context).inflate(layout, this, false)
     }
 
-    //bind or maps data to the children
+    //maps data to the children
     override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
 
         //it is called for each row
@@ -47,7 +52,7 @@ class RecyclerViewAdapter(private val messageDataSet: MutableList<Message>) :
         holder.timeStampTextView.text = message.timestamp?.dateToString("hh:mm a E dd-MMM")
 
     }
-
+*/
 
 //extension function on a date class
     private fun Date.dateToString(format: String): String {
@@ -73,5 +78,51 @@ class RecyclerViewAdapter(private val messageDataSet: MutableList<Message>) :
 
     }
 
+
+
+    ///view holder
+
+   inner class InComingViewHolder(var itemBinding: IncomingMsgLayoutBinding) : RecyclerView.ViewHolder(itemBinding.root)
+
+   inner class OutComingViewHolder(var itemBinding: OutgoingMsgLayoutBinding):RecyclerView.ViewHolder(itemBinding.root)
+
+
+    override fun getItemViewType(position: Int): Int {
+        val user = messageDataSet[position].from!!
+      return  if( FirebaseAuth.getInstance().currentUser?.displayName == user) 1 else 2
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+
+        if(viewType == 1){
+            val inComingBinding: IncomingMsgLayoutBinding = DataBindingUtil.inflate(inflater,R.layout.incoming_msg_layout,parent,false)
+            return InComingViewHolder(inComingBinding)
+        } else{
+            val outBinding: OutgoingMsgLayoutBinding = DataBindingUtil.inflate(inflater,R.layout.outgoing_msg_layout,parent,false)
+            return OutComingViewHolder(outBinding)
+        }
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val viewType = getItemViewType(position)
+        val msg = messageDataSet[position]
+        if(viewType == 1){
+            (holder as InComingViewHolder).itemBinding.msg = msg
+            holder.itemBinding.msgFrom.visibility = if(msg.from == previousMsg(position)) View.GONE else View.VISIBLE
+        }else{
+
+            (holder as OutComingViewHolder).itemBinding.msg = msg
+            holder.itemBinding.msgFrom.visibility = if(msg.from == previousMsg(position)) View.GONE else View.VISIBLE
+        }
+    }
+
+
+    fun previousMsg(position: Int):String?{
+        val prevPos = position - 1
+        val msg = messageDataSet[ if(prevPos < 0) 0 else prevPos]
+       return msg.from
+    }
 
 }
